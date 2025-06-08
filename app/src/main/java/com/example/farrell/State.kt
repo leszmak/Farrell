8520df47613fa8a1c60541a1e7494f62b0717f37
leszmak <n.szymon23@gmail.com>
2025-06-08 18:18:35 +0200

    State

---
+++ app/src/main/java/com/example/farrell/State.kt
@@ -0,0 +1,20 @@
+package com.example.farrell
+
+import androidx.compose.runtime.mutableStateOf
+
+//class State {
+//    var lockList by mutableStateOf(listOf<Lock>())
+//        private set
+//
+//    fun addLock(lock: Lock) {
+//        lockList = lockList + lock
+//    }
+//
+//    fun removeLock(lock: Lock) {
+//        lockList = lockList - lock
+//    }
+//
+//    fun clearLocks() {
+//        lockList = emptyList()
+//    }
+//}
